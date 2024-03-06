0419a1975fc88c58d22cf445f28ad33ec2f87992
Triston Hardcastle Peck <cooldude101triston@gmail.com>
2024-03-05 16:36:58 -0800

    created CompressorTest.py
    preliminary test combining connecting to the controller and running a function on the pi

---
+++ CompressorCodeTest.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+from turtle import delay 
+import RPi.GPIO
+import time
+
+host = ""
+port = 5001
+host_addr = (host, port)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+gpio.setmode(gpio.BOARD)
+forward_pin = 10
+gpio.setup(forward_pin,gpio.OUT)
+
+try:
+	sock.bind(host_addr)
+except:
+	#print(f"Bind failed. Error code: {message[0]} Message {message[1]}")
+	print("Bind failed")
+	sys.exit()
+
+print("socket bound")
+
+sock.listen(9)
+print(f"Listening on {host}:{port}")
+
+conn, addr = sock.accept()
+print(f"Connected with {addr[0]}:{addr[1]}")
+
+msg = conn.recv(4).decode("utf-8")
+
+if msg == ":)":
+	gpio.output(forward_pin, gpio.HIGH)
+	delay(5000)
+	gpio.output(forward_pin, gpio.LOW)
+	
+
+print(f"[{addr}] {msg}")
