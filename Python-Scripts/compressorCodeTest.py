dbe121ca3a3cab5e0fa44bb7a9db42a7b673a84e
cmullins00 <connermullins00@gmail.com>
2024-03-21 23:50:23 -0700

    Uploading the test.py program

--- Python-Scripts/compressorCodeTest.py
+++ Python-Scripts/compressorCodeTest.py
@@ -7,11 +7,14 @@ host = ""
 port = 5001
 host_addr = (host, port)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 GPIO.setmode(GPIO.BOARD)
 forward_pin = 10
 GPIO.setup(forward_pin, GPIO.OUT)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.settimeout(None)
+#sock.setblocking(False)
 
 try:
 	sock.bind(host_addr)
@@ -28,17 +31,28 @@ print(f"Listening on {host}:{port}")
 conn, addr = sock.accept()
 print(f"Connected with {addr[0]}:{addr[1]}")
 run = True
+on = "ON"
+off = "OFF"
+end = "END"
 while(run == True):
-	msg = conn.recv(4).decode("utf-8")
+	try:
+		print("Listening for message")
+		msg = conn.recv(1024).decode("utf-8")
+	except KeyboardInterrupt:
+		break
 
-	if msg == "ON":
+	msg = msg.strip()
+	print("Message:", msg)
+
+	if msg == on:
 		GPIO.output(forward_pin, GPIO.HIGH)
 		print("Turned compressor on")
-	elif msg == "OFF":
+	elif msg == off:
 		GPIO.output(forward_pin, GPIO.LOW)
 		print("Turned compressor off")
-	elif msg == "END":
+	elif msg == end:
+		print("ending the program")
 		run = False
 
-
-GPIO.cleanup()
+conn.close()
+gpio.cleanup()
---
+++ Python-Scripts/test.py
@@ -0,0 +1,66 @@
+import socket
+import sys
+from time import sleep
+
+def main():
+	host = ""
+	port = 5001
+	host_addr = (host, port)
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	#try:
+	#	sock.bind(host_addr)
+	#	print("Socket bound")
+	#except:
+	#	print("Bind failed")
+	#	sock.close()
+	#	sys.exit()
+
+	#	sock.listen(1)
+	#	print(f"Listening on {host}:{port}")
+
+	while True:
+		try:
+			sock.bind(host_addr)
+			print("Socket bound")
+		except:
+			print("Bind failed")
+			sock.close()
+			sys.exit()
+
+		sock.listen(1)
+		print(f"Listening on {host}:{port}")
+
+		conn, addr = sock.accept()
+		print(f"Connected with {addr[0]}:{addr[1]}")
+
+		while True:
+			handle_client(conn)
+
+		conn.close()
+
+	sock.close()
+
+
+def handle_client(conn):
+	while(True):
+		print("Listening for message...")
+		msg = conn.recv(2048).decode("utf-8")
+
+		print("Message: ", msg)
+
+		on = "ON"
+		off = "OFF"
+		end = "END"
+
+		if msg == on:
+			print("Turned compressor on")
+		elif msg == off:
+			print("Turned compressor off")
+		elif msg == end:
+			print("Ending the connection")
+			break
+
+if __name__ == "__main__":
+	main()
