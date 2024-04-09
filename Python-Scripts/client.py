dfd826dba6bff4302a65cabfd1ac12d7456eb59f
Conner M <connermullins00@gmail.com>
2024-04-09 16:22:38 -0700

    Updated the test client program named client.py to mimic the controller

--- Python-Scripts/client.py
+++ Python-Scripts/client.py
@@ -1,10 +1,32 @@
 import socket
-import sys
+from time import sleep
 
-s = socket.socket()
-host = "127.0.0.1"
-port = 5001
+def main():
+    host = "192.168.0.100"
+    port = 5001
 
-s.connect((host, port))
-print(host)
-s.close
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((host, port))
+        print("Connected to server.")
+
+        while True:
+            message = "ON"
+            client.sendall(message.encode())
+            sleep(1)
+
+            message = "OFF"
+            client.sendall(message.encode())
+            sleep(1)
+
+
+    except KeyboardInterrupt:
+        print("Closing the connection")
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        client.close()
+
+if __name__ == "__main__":
+    main()
